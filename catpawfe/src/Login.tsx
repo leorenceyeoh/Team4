f52c8ebe3cb58a12d5036d4ab6f518fecba58fa9
Nicholas Guohao Chen <nicholas.guohao.chen@avanade.com>
2023-08-29 16:53:02 +0800

    Added Login Screen

---
+++ catpawfe/src/Login.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you can handle the login logic, like sending the username and password to the server
+    };
+
+    return (
+        <div className="login-container">
+            <h1>Your Logo here</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login
